4ff6b6b5b74cf2766177ad7e5a4959ae7e02e01c
Daniel Švub <daniel.svub@spongedata.cz>
2023-04-26 15:31:09 +0200

    Added frame implementation

--- structures/tape.go
+++ structures/tape.go
@@ -1,6 +1,7 @@
 package structures
 
 import (
+	"errors"
 	"io"
 
 	"github.com/SpongeData-cz/gonatus"
@@ -37,15 +38,91 @@ func NewRAMTape[T comparable](conf gonatus.Conf) *RAMTape[T] {
 	return ego
 }
 
+/*
+Interface for a frame.
+
+Extends:
+  - Gobjecter.
+
+Type parameters:
+  - T - type of the frame elements.
+*/
 type Framer[T comparable] interface {
 	gonatus.Gobjecter
 	Next() error
 	Load() ([]T, error)
 }
 
+/*
+Structure representing the frame - sliding window on the tape.
+
+Implements:
+  - Framer.
+
+Type parameters:
+  - T - type of the frame elements.
+*/
 type Frame[T comparable] struct {
 	gonatus.Gobject
-	Taper[T]
-	offset int
-	size   int
+	Tape     Taper[T]
+	Offset   int // Start of frame
+	Size     int // Actual number of loaded elements
+	Capacity int // Maximum number of elements that can be loaded
+}
+
+/*
+Creates a new frame.
+
+Parameters:
+  - conf - Gonatus configuration structure.
+
+Type parameters:
+  - T - type of the frame elements.
+
+Returns:
+  - pointer to the new frame.
+*/
+func NewFrame[T comparable](conf gonatus.Conf) *Frame[T] {
+	ego := &Frame[T]{}
+	ego.Init(ego, conf)
+	return ego
+}
+
+/*
+Shifts the frame offset by the number of the lastly loaded elements.
+Increases the offset by size and sets size to 0.
+
+Returns:
+  - error, if no elements were loaded since last usage of this method.
+*/
+func (ego *Frame[T]) Next() error {
+
+	if ego.Size == 0 {
+		return errors.New("No values loaded since last call.\n")
+	}
+
+	ego.Offset += ego.Size
+	ego.Size = 0
+	return nil
+}
+
+/*
+Loads the maximum possible amount of elements into the slice and returns it.
+Also updates the size to number of actually loaded elements (0 <= size <= capacity).
+
+Returns:
+  - slice of loaded elements,
+  - error, if any occurred.
+*/
+func (ego *Frame[T]) Load() ([]T, error) {
+
+	s := make([]T, ego.Capacity)
+
+	size, err := ego.Tape.Read(s)
+	if err != nil {
+		return nil, err
+	}
+
+	ego.Size = size
+	return s[:size], nil
 }
