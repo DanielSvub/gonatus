076b4eb82565942bb803a51ceebf4b9646035b77
Daniel Švub <daniel.svub@spongedata.cz>
2023-05-10 14:39:11 +0200

    Added merge stream

---
+++ streams/merge.go
@@ -0,0 +1,83 @@
+package streams
+
+import "errors"
+
+type MergeStreamer[T any] interface {
+	OutputStreamer[T]
+	true() InputStreamer[T]
+	false() InputStreamer[T]
+	Close()
+}
+
+type RRMergeStream[T comparable] struct {
+	Stream[T]
+	sources   []InputStreamer[T]
+	currIndex int
+	autoclose bool
+}
+
+func NewRRMergeStream[T comparable](autoclose bool) *RRMergeStream[T] {
+	ego := &RRMergeStream[T]{
+		autoclose: autoclose,
+	}
+	ego.init(ego)
+	return ego
+}
+
+func (ego *RRMergeStream[T]) setSource(s InputStreamer[T]) {
+	ego.sources = append(ego.sources, s)
+}
+
+func (ego *RRMergeStream[T]) unsetSource(s InputStreamer[T]) {
+	for i, source := range ego.sources {
+		if source == s {
+			ego.sources = append(ego.sources[:i], ego.sources[i+1:]...)
+			break
+		}
+	}
+}
+
+func (ego *RRMergeStream[T]) get() (T, error) {
+
+	if len(ego.sources) == 0 {
+		return *new(T), errors.New("The stream is closed.")
+	}
+	val, err := ego.sources[ego.currIndex].get()
+
+	if ego.sources[ego.currIndex].Closed() {
+		ego.unsetSource(ego.sources[ego.currIndex])
+		if len(ego.sources) == 0 {
+			if ego.autoclose {
+				ego.closed = true
+			}
+			return val, nil
+		}
+	}
+
+	if ego.currIndex == len(ego.sources)-1 {
+		ego.currIndex = 0
+	} else {
+		ego.currIndex++
+	}
+
+	if err != nil {
+		return *new(T), err
+	}
+
+	return val, nil
+}
+
+func (ego *RRMergeStream[T]) Close() {
+	if ego.autoclose {
+		panic("Cannot close explicitly, autoclose is active.")
+	}
+	ego.closed = true
+}
+
+func (ego *RRMergeStream[T]) Pipe(s OutputStreamer[T]) InputStreamer[T] {
+	return pipe[T](ego, s)
+}
+
+func (ego *RRMergeStream[T]) Split(s SplitStreamer[T]) (trueStream InputStreamer[T], falseStream InputStreamer[T]) {
+	return split[T](ego, s)
+}
--- streams/streams_test.go
+++ streams/streams_test.go
@@ -8,7 +8,7 @@ import (
 	. "github.com/SpongeData-cz/gonatus/streams"
 )
 
-func TestStreams(t *testing.T) {
+func TestBasic(t *testing.T) {
 
 	t.Run("read", func(t *testing.T) {
 
@@ -49,6 +49,9 @@ func TestStreams(t *testing.T) {
 		}
 
 	})
+
+}
+func TestParallel(t *testing.T) {
 
 	t.Run("async", func(t *testing.T) {
 
@@ -109,7 +112,7 @@ func TestStreams(t *testing.T) {
 			defer wg.Done()
 			result, err := ost.Collect()
 			if err != nil || len(result) != 5 {
-				t.Error("Collecting the results in parallel was unsuccessful.")
+				t.Error("Collecting the results from the split stream was unsuccessful.")
 			}
 		}
 
@@ -117,7 +120,7 @@ func TestStreams(t *testing.T) {
 			defer wg.Done()
 			result, err := osf.Collect()
 			if err != nil || len(result) != 5 {
-				t.Error("Collecting the results in parallel was unsuccessful.")
+				t.Error("Collecting the results from the split stream was unsuccessful.")
 			}
 		}
 
@@ -127,6 +130,51 @@ func TestStreams(t *testing.T) {
 		wg.Wait()
 
 	})
+
+	t.Run("merge", func(t *testing.T) {
+
+		is1 := NewBufferInputStream[int](5)
+		is2 := NewBufferInputStream[int](5)
+		ms := NewRRMergeStream[int](true)
+		os := NewReadableOutputStream[int]()
+
+		var wg sync.WaitGroup
+		wg.Add(3)
+
+		write1 := func() {
+			defer wg.Done()
+			defer is1.Close()
+			is1.Write(1, 3, 5, 7, 9)
+		}
+
+		write2 := func() {
+			defer wg.Done()
+			defer is2.Close()
+			is2.Write(2, 4, 6, 8, 10)
+		}
+
+		is1.Pipe(ms)
+		is2.Pipe(ms)
+		ms.Pipe(os)
+
+		read := func() {
+			defer wg.Done()
+			result, err := os.Collect()
+			if err != nil || len(result) != 10 {
+				t.Error("Collecting the results from the merge stream was unsuccessful.")
+			}
+		}
+
+		go write1()
+		go write2()
+		go read()
+		wg.Wait()
+
+	})
+
+}
+
+func TestNdjson(t *testing.T) {
 
 	t.Run("ndjsonInput", func(t *testing.T) {
 
@@ -150,6 +198,16 @@ func TestStreams(t *testing.T) {
 
 	})
 
+	t.Run("ndjsonOutput", func(t *testing.T) {
+
+		ndi := NewNdjsonInputStream("fixtures/example.ndjson")
+
+		ndo := NewNdjsonOutputStream("fixtures/test_.ndjson", FileWrite)
+
+		ndi.Pipe(ndo)
+
+	})
+
 	t.Run("ndjsonOutputTs", func(t *testing.T) {
 
 		ndi := NewNdjsonInputStream("fixtures/example.ndjson")
@@ -161,16 +219,6 @@ func TestStreams(t *testing.T) {
 		ndo := NewNdjsonOutputStream("fixtures/test.ndjson", FileWrite)
 
 		ndi.Pipe(ts).Pipe(ndo)
-
-	})
-
-	t.Run("ndjsonOutput", func(t *testing.T) {
-
-		ndi := NewNdjsonInputStream("fixtures/example.ndjson")
-
-		ndo := NewNdjsonOutputStream("fixtures/test_.ndjson", FileWrite)
-
-		ndi.Pipe(ndo)
 
 	})
 
